be4892fb2be6302d265c631b3e27b789262cbf3d
Militum <wind.of.second@gmail.com>
2021-08-08 21:02:46 +0900

    [fix]URL入力時のエラー原因を表示するように変更

--- src/logics/UrlCheck.js
+++ src/logics/UrlCheck.js
@@ -1,10 +1,15 @@
+import ValidationError, { VALIDATION_ERROR_ANCHOR, VALIDATION_ERROR_DOMAIN } from "./ValidationError";
+
 function CheckUrl(url) {
     const domain = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
-    console.log(domain);
     if (domain !== "charasheet.vampire-blood.net") {
-        return false;
+        throw new ValidationError(VALIDATION_ERROR_DOMAIN);
     }
-    return true;
+    const anchors = url.split('#');
+    console.log(anchors);
+    if (anchors.length >= 2) {
+        throw new ValidationError(VALIDATION_ERROR_ANCHOR);
+    }
 }
 
 export default CheckUrl;
